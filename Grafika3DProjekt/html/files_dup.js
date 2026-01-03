3e2c3cd01ba4d1ee17c382c6dfb95d0c3b6f10df
KamilJop <kamiljop434@gmail.com>
2026-01-03 14:59:31 +0100

    Remove generated Doxygen documentation files
    Deleted HTML and LaTeX files generated by Doxygen from the Grafika3DProjekt project. This cleans up the repository by removing auto-generated documentation artifacts.

--- Grafika3DProjekt/html/files_dup.js
+++
@@ -1,5 +0,0 @@
-var files_dup =
-[
-    [ "resource.h", "resource_8h_source.html", null ],
-    [ "resource1.h", "resource1_8h_source.html", null ]
-];
--- Grafika3DProjekt/html/navtreeindex0.js
+++
@@ -1,8 +0,0 @@
-var NAVTREEINDEX0 =
-{
-"files.html":[0,0],
-"index.html":[],
-"pages.html":[],
-"resource1_8h_source.html":[0,0,1],
-"resource_8h_source.html":[0,0,0]
-};
